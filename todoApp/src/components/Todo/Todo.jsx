6e12a491431613540ef023e080f390c1a0cfe3eb
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2023-07-16 15:56:41 +0100

    todo ui finalized

--- todoApp/src/components/Todo/Todo.jsx
+++ todoApp/src/components/Todo/Todo.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Todo.css'
+import {BsFillCheckCircleFill, BsCircle, BsFillTrash3Fill, BsRepeat } from 'react-icons/bs'
+
 
 function Todo({todo}) {
+  const [hover, setHover] = useState(false)
   return (
     <div className='_todo'>
-      {todo.text}
+      <div 
+      className="todoContainer"
+      onMouseEnter={()=>setHover(true)}
+      onMouseLeave={()=>setHover(false)}
+      >
+        <div className="check">
+          {
+            todo.checked?
+            <span className='checked'>
+              <BsFillCheckCircleFill color='#bebebe'/>
+            </span>
+            :
+            <span>
+              <BsCircle color={todo.color}/>
+            </span>
+          }
+        </div>
+        <div className="todoText">
+          <p style={{color: todo.checked? '#bebebe' : '#000'}}>{todo.text}</p>
+          <span>{todo.time} - {todo.project}</span>
+          <div className={`line ${todo.checked? 'line_through' : ''}`}></div>
+        </div>
+        <div className="todoControl">
+          <div className="addToNextDay">
+            {
+              todo.checked &&
+              <span><BsRepeat /></span>
+            }
+          </div>
+          <div className="deleteTodo">
+            {
+              (hover || todo.checked) &&
+              <span><BsFillTrash3Fill /></span>
+            }
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
